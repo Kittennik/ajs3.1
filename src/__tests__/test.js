c23c242369e0898154bab2aae8e5b79d8a21cfee
Kate Pronina <kateadm@narod.ru>
2020-03-06 15:13:45 +0300

    Выполненное задание

---
+++ src/__tests__/test.js
@@ -0,0 +1,31 @@
+import personHealth from '../js/app.js';
+
+test('Здоровый', () => {
+  const person = {
+    name: 'Маг',
+    health: 65,
+  };
+  const received = personHealth(person);
+  const expected = 'healthy';
+  expect(received).toBe(expected);
+});
+
+test('Раненый', () => {
+  const person = {
+    name: 'Маг',
+    health: 45,
+  };
+  const received = personHealth(person);
+  const expected = 'wounded';
+  expect(received).toBe(expected);
+});
+
+test('При смерти', () => {
+  const person = {
+    name: 'Маг',
+    health: 10,
+  };
+  const received = personHealth(person);
+  const expected = 'critical';
+  expect(received).toBe(expected);
+});
